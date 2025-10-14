2d5636836992336920e0155e2130d10fd703a66d
Il Tuo Nome <tuo.email@esempio.com>
2025-10-14 08:48:40 +0200

    Fix: Worker now listens to all task queues (documents, maintenance, celery)
    - Add -Q flag to specify queues explicitly
    - Fixes issue where tasks were not being registered/processed
    - Worker will now pick up tasks from all configured queues

--- start_worker.sh
+++ start_worker.sh
@@ -8,4 +8,4 @@ echo "[Worker] Waiting 2 seconds for healthcheck to start..."
 sleep 2
 
 echo "[Worker] Starting Celery worker..."
-celery -A celery_config worker --loglevel=info --concurrency=2
+celery -A celery_config worker --loglevel=info --concurrency=2 -Q documents,maintenance,celery
